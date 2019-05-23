5e0233357f9885775428919d4df33ab0b6fcc443
Manu-999 <manuelbalbas@gmail.com>
2019-05-23 10:47:58 +0200

    added footer icons and new color to main backgorund

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,6 +6,7 @@ function Footer() {
         <div className="footer">
             <a href="https://github.com/Manu-999"><i className="fab fa-github social-icon-footer"></i></a>
             <a href="https://www.linkedin.com/in/manuel-balbas-arribas/"><i className="fab fa-linkedin social-icon-footer"></i></a>
+            <a href="https://twitter.com/manu_ba9?lang=es"><i className="fab fa-twitter social-icon-footer"></i></a>
         </div>
     )
 }
--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import Menu from '../Menu.jsx';
 import './Layout.scss';
 
+
 export function Layout(props) {
+    
     return (
         <div className="layout">
             <Menu />
-            <div className="main">
+            <div className="main" id="main">
                 {props.children}
             </div>
         </div>
