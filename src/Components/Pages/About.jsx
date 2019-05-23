985ce822088d4207566721eff04bf26c0e0355e6
Manu-999 <manuelbalbas@gmail.com>
2019-05-23 12:51:06 +0200

    added css to about section

--- src/Components/Pages/About.jsx
+++ src/Components/Pages/About.jsx
@@ -4,11 +4,11 @@ import './About.scss';
 function About() {
     return (
         <div className="about">
-            <p className="intro">Apasionado de la tecnología e internet. Budcando formarme y desarrollarme como profesional del desarrollo web. Enganchado a Javascript y a las tecnologías Open Source. Me gusta trabajar en entornos dinámicos y en constante evolución.</p>
+            <h1 className="about-title">¿Quién soy?</h1>
+            <p className="intro">Apasionado de la tecnología e internet. Buscando formarme y desarrollarme como profesional del desarrollo web. Enganchado a Javascript y a las tecnologías Open Source. Me gusta trabajar en entornos dinámicos y en constante evolución.</p>
             <p className="knowledge">Manejo tecnologías como React, Angular, Sass, NPM, NodeJS, Express, MongoDB, Docker, Git</p>
         </div>
     )
 }
-
 
 export default About;
